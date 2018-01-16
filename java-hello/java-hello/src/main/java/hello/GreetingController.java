3dde0d780f7d452aec9bff65f2fdc576ea604587
lauverrec <lauraverarec@gmail.com>
2018-01-16 23:54:57 +0100

    Cambios pedidos por el ejercicio 2 corregido

--- java-hello/java-hello/src/main/java/hello/GreetingController.java
+++ java-hello/java-hello/src/main/java/hello/GreetingController.java
@@ -9,9 +9,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="Universe") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
 
 }
+
+   @RequestMapping("/bye")
+    public String bye(@RequestParam(value="name", required=false, defaultValue="Universe") String name, Model model) {
+        model.addAttribute("name", name);
+        return "bye";
+    }
